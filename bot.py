ec40953afbf5c2603f0b1a7987ee6e0296366c86
Josh Jacobs <josh@joshjacobs.net>
2020-10-04 16:08:02 +0100

    Add Rules Assignation for Those Who Accept

--- bot.py
+++ bot.py
@@ -35,5 +35,15 @@ async def on_message(message):
         response = 'Actually, since being a war hero in the Falklands, I can\'t sweat.'
         await message.channel.send(response)
 
+    # controls role assignment for those who accept
+    if message.channel.name == 'mods-discussion' and message.content == '!acceptrules':
+
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+
+        member = guild.get_member(message.author.id)
+
+        await member.add_roles(guild.get_role(762318229514485801), reason=f'User {message.author} accepted rules')
 
 client.run(TOKEN)
